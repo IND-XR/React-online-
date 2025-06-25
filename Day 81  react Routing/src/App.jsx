f3885075ffbbdacaae518f00d2df8b3610d538bd
insta <anmolpreetsingh510@gmail.com>
2025-06-25 09:02:20 +0530

    81. Starting a react routing ( Create a multiple component & Adding a multiple component   ) 2

--- Day 81  react Routing/src/App.jsx
+++ Day 81  react Routing/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Route,  Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Product from './components/Product'
+import Server from './components/Server'
+import Nav from './components/Nav'
 
 const App = () => {
+  
   return (
-    <div>
-      <h1>App</h1>
+    <div className="w-screen bg-gray-800 text-white  mx-auto">
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about " element={<About/>} />
+        <Route path="/product " element={<Product/>} />
+        <Route path="/service" element={<Server/>} />
+      </Routes>
     </div>
   )
 }
---
+++ Day 81  react Routing/src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <div className="flex justify-center gap-10 p-5">
+        <Link to="/"> Home  </Link>
+        <Link to="/product"> product </Link>
+        <Link to="/service"> service </Link>
+        <Link to="/about"> about  </Link>
+               
+                
+    </div>
+  )
+}
+
+export default Nav
