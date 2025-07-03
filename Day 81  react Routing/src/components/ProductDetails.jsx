4f7ee9e13f7e52381a3d40c5c82693b857e5e6de
insta <anmolpreetsingh510@gmail.com>
2025-07-03 12:29:46 +0530

    82. TEST Done 2

--- Day 81  react Routing/src/components/ProductDetails.jsx
+++ Day 81  react Routing/src/components/ProductDetails.jsx
@@ -7,9 +7,28 @@ export const ProductDetails = () => {
 
   return (
     <div>
-        <h1 className='text-4xl font-thin mb-3'> product name</h1>
+      <div>
+          <h1 className='text-4xl font-thin mb-3'> product name</h1>
         <h2 className='text-2xl font-thin mb-5'>Product deatil</h2>
         <button onClick={()=>{navigate(-1)}} className='bg-white text-black px4 py-2 rounded'> go back </button>
+
+
+      </div>
+      
+        <div>
+          <h1 className='text-4xl font-thin mb-3'> product name</h1>
+        <h2 className='text-2xl font-thin mb-5'>Product deatil</h2>
+        <button onClick={()=>{navigate(-1)}} className='bg-white text-black px4 py-2 rounded'> go back </button>
+        </div>
+
+         <div>
+          <h1 className='text-4xl font-thin mb-3'> product name</h1>
+        <h2 className='text-2xl font-thin mb-5'>Product deatil</h2>
+        <button onClick={()=>{navigate(-1)}} className='bg-white text-black px4 py-2 rounded'> go back </button>
+        </div>
+    
+
     </div>
+
   )
 }
