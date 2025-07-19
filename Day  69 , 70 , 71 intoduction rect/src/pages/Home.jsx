aca9aacef52607ca85caecdb441b39c6f5ecba87
insta <anmolpreetsingh510@gmail.com>
2025-07-19 19:24:40 +0530

    88.React Lifecycle, API integration & project Build ( How Use Effect Work  & component life cycle & useEffect ) 2

--- Day  69 , 70 , 71 intoduction rect/src/pages/Home.jsx
+++ Day  69 , 70 , 71 intoduction rect/src/pages/Home.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import axios from '../utils/axios';
 
-
 const Home = () => {
-
   const getproduct = async () =>{
     try{
       const {response} = await axios.get("/products/1"); // this is API Link
       console.log(response.data)
-
       //fetch
       // const stradata = await fetch('https://fakestoreapi.com/products/1')
       // const jsondata = await stradata.json()
-      // console.log(jsondata)
-            
+      // console.log(jsondata)         
     }
     catch(error){
       console.log(error)
     }
   }
+
+  useEffect(()=>{
+    console.log("Home.jsx Mounted");
+
+    getproduct();
+
+    return ( ) =>{
+      console.log("Home.jsx UnMounted")
+    }
+  })
 
   return (
     <div>
@@ -27,5 +33,4 @@ const Home = () => {
     </div>
   )
 }
-
 export default Home
