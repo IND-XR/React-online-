5fcbcbebeee2169ab65a93d4c22aba40b350bffb
insta <anmolpreetsingh510@gmail.com>
2025-08-12 03:22:09 +0530

    94 project III ( Create a Redux file structure ) using redux website

---
+++ Project3/frontend/src/store/store.jsx
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+eslint-disable-next-line react-refresh/only-export-components
+export default configureStore({
+  reducer: {}
+})
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import counterReducer from '../features/counterSlice'; // example slice
+
+// export default configureStore({
+//   reducer: {
+//     // counter: counterReducer
+//   }
+// });
