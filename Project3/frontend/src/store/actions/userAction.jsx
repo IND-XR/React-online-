45eea5bca3d78d504b96eb353101d3771605c0e9
insta <anmolpreetsingh510@gmail.com>
2025-08-23 18:10:05 +0530

    95 Project III set up  useDispatch Working  properly

--- Project3/frontend/src/store/actions/userAction.jsx
+++ Project3/frontend/src/store/actions/userAction.jsx
@@ -1,23 +1,19 @@
-// import axios from "axios";  
+// import axios from "axios";
 import axios from "../../api/axiosconfig";
 export const asyncregisterusers = (user) => async (dispatch, getstate) => {
-    try {
+  try {
+    // Only send plain values, not DOM nodes
+    const payload = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    };
+    console.log("Sending user:", payload);
 
-        // Only send plain values, not DOM nodes
-    // const payload = {
-    //   id: user.id,
-    //   name: user.name,
-    //   email: user.email,
-    //   password: user.password,
-    // };
-
-    // console.log("Sending user:", payload);
-
-        const res = await axios.post("/users", user);
-        console.log("Response : ",res)
-        
-    } catch (error) {
-        console.log("Errors",error)
-        
-    }
-}
+    const res = await axios.post("/users", user);
+    console.log("Response : ", res);
+  } catch (error) {
+    console.log("Errors", error);
+  }
+};
